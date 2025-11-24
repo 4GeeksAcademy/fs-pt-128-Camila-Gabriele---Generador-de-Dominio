4c2f574395e37781944ea40be039752085355720
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-11-24 11:35:57 +0000

    cambios

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let dom = ['.com', '.net', '.org', '.us', '.io'];
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            console.log(pronoun[i]+adj[j]+noun[k]+'.com');
+        for (let k = 0; k < noun.length; k++) {     
+            for (let d = 0; d < dom.length; d++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + dom[d]);
+            }
         }
     }
 }
